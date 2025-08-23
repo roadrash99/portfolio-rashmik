223e6edf6cb87918943ef46f8197da9357daa715
roadrash99 <rashmik.manchiraju7@gmail.com>
2025-08-23 15:28:33 -0500

    tech change

--- src/components/Technologies.jsx
+++ src/components/Technologies.jsx
@@ -9,6 +9,9 @@ import {
   SiTypescript,
   SiDocker,
   SiKubernetes,
+  SiPython,
+  SiJava,
+  SiJavascript,
 } from "react-icons/si";
 
 const Technologies = () => {
@@ -16,50 +19,43 @@ const Technologies = () => {
     <div id="tech" className="border-b border-neutral-800 pb-24">
       <h2 className="my-20 text-center text-4xl">Technologies</h2>
       <div className="flex flex-wrap items-center justify-center gap-4">
-
         <div className="rounded-2xl border-4 border-neutral-800 p-4">
           <SiJava className="text-7xl text-red-700" />
         </div>
-
         <div className="rounded-2xl border-4 border-neutral-800 p-4">
-          <SiSpringboot className="text-7xl text-green-700" />
+          <SiPython className="text-7xl text-yellow-400" />
+        </div>
+        <div className="rounded-2xl border-4 border-neutral-800 p-4 flex flex-col items-center">
+          <SiJavascript className="text-7xl text-purple-600" />
+          <span className="text-xs mt-2">LangChain</span>
         </div>
         <div className="rounded-2xl border-4 border-neutral-800 p-4">
           <SiTypescript className="text-7xl text-blue-600" />
         </div>
-
         <div className="rounded-2xl border-4 border-neutral-800 p-4">
           <RiReactjsLine className="text-7xl text-cyan-400" />
         </div>
-
         <div className="rounded-2xl border-4 border-neutral-800 p-4">
           <SiAngular className="text-7xl text-red-600" />
         </div>
-
         <div className="rounded-2xl border-4 border-neutral-800 p-4">
           <SiNodedotjs className="text-7xl text-green-500" />
         </div>
-
         <div className="rounded-2xl border-4 border-neutral-800 p-4">
           <SiExpress className="text-7xl text-gray-500" />
         </div>
-
         <div className="rounded-2xl border-4 border-neutral-800 p-4">
           <SiMysql className="text-7xl text-blue-600" />
         </div>
-
         <div className="rounded-2xl border-4 border-neutral-800 p-4">
           <SiMongodb className="text-7xl text-green-600" />
         </div>
-
         <div className="rounded-2xl border-4 border-neutral-800 p-4">
           <SiPostgresql className="text-7xl text-blue-500" />
         </div>
-
         <div className="rounded-2xl border-4 border-neutral-800 p-4">
           <SiDocker className="text-7xl text-blue-400" />
         </div>
-
         <div className="rounded-2xl border-4 border-neutral-800 p-4">
           <SiKubernetes className="text-7xl text-blue-500" />
         </div>
